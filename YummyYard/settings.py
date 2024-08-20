c37a46e0ed62d13a34defed3522d321a55c042b1
mighty <mightyrajukushwaha@gmail.com>
2024-08-20 14:28:36 +0545

    add

--- YummyYard/settings.py
+++ YummyYard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'home'
+    'home',
+    'home.onrender.com'
     ]
 
 
@@ -88,7 +90,8 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME' : 'yummyYard',
         'USER' : 'root',
         'PASSWORD' : 'mighty@098',
@@ -137,6 +140,8 @@ STATIC_URL = 'static/'
 STATIC_DIR = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
